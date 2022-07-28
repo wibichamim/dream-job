8569ac6542ecb204ceabbb40eecc0ab8ed10ef1e
Wibi Chamim M <wibi.mushodiq@students.amikom.ac.id>
2022-07-28 15:48:09 +0700

    Finishing Second Page

--- lib/pages/categories_page.dart
+++ lib/pages/categories_page.dart
@@ -1,3 +1,4 @@
+import 'package:dream_job/widgets/job_card.dart';
 import 'package:flutter/material.dart';
 import 'package:dream_job/themes.dart';
 
@@ -48,9 +49,73 @@ class Categories extends StatelessWidget {
       );
     }
 
+    Widget listJob() {
+      return Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Just Posted',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Column(
+                  children: const [
+                    JobCard(
+                      imageurl: 'assets/images/icon_google.png',
+                      company: 'Google',
+                      jobDesc: 'Front-End Developer',
+                    ),
+                    SizedBox(
+                      height: 35,
+                    ),
+                    JobCard(
+                      imageurl: 'assets/images/icon_instagram.png',
+                      company: 'Instagram',
+                      jobDesc: 'UI Designer',
+                    ),
+                    SizedBox(
+                      height: 35,
+                    ),
+                    JobCard(
+                      imageurl: 'assets/images/icon_facebook.png',
+                      company: 'Facebook',
+                      jobDesc: 'Data Scientist',
+                    ),
+                    SizedBox(
+                      height: 35,
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      );
+    }
+
     return Scaffold(
-      body: Column(
-        children: [header()],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            header(),
+            const SizedBox(
+              height: 30,
+            ),
+            listJob(),
+            const SizedBox(
+              height: 30,
+            ),
+            listJob(),
+          ],
+        ),
       ),
     );
   }
